e9751629f0f340e33f5cad99a679733b26ec5988
Maxime Salomon <maxime@croissant.io>
2019-03-27 16:02:25 +0100

    added index.js + GET cohorts

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3',
+  },
+  useNullAsDefault: true,
+};
+
+const db = knex(knexConfig);
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+// GET /api/cohorts
+server.get('/api/cohorts', async (req, res) => {
+  try {
+    const cohorts = await db('cohorts');
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\nServer is running on PORT ${port}\n`));
