92876433314b47fd1416dae51a67c5416c23ce6d
Maxime Salomon <maxime@croissant.io>
2019-03-27 15:11:34 +0100

    migrate cohorts db

--- data/migrations/20190327150141_cohorts.js
+++ data/migrations/20190327150141_cohorts.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('cohorts', table => {
+        table.increments();
+        table.string('name', 128).notNullable();
+        table.timestamps(true, true);
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTableIfExists('cohorts');
 };
